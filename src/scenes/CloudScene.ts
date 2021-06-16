45a7d42cc59caf119df58791ee6c68013d6d84c6
Kostas Paganelis <paganelis@gmail.com>
2021-06-16 20:29:50 +0300

    Spawning max 5 random clouds on scene on random places

--- src/scenes/CloudScene.ts
+++ src/scenes/CloudScene.ts
@@ -1,10 +1,8 @@
 import Phaser from 'phaser'
-import '../sprites/Cloud'
+import Cloud from '../sprites/Cloud'
 
 export default class CloudScene extends Phaser.Scene {
-    // private cloudTypes = ['cumulus-huge', 'cumulus-big1', 'cumulus-big2', 'cumulus-big3', 'cumulus-small1', 'cumulus-small2', 'cumulus-small3'];
-
-    private group;
+    private group!: Phaser.Physics.Arcade.Group;
 
     constructor() {
         super('cloud-scene')
@@ -14,33 +12,35 @@ export default class CloudScene extends Phaser.Scene {
     }
 
     create() {
-        // c: Cloud;
-        this.add.cloud();
-
-/*        this.group = this.physics.add.group({
-            defaultKey: 'clouds',
+        this.group = this.physics.add.group({
             classType: Cloud,
-            maxSize: 100,
+            maxSize: 5,
+            createCallback: (item: Phaser.GameObjects.GameObject) => {
+                let cloud = item as Cloud;
+                cloud.createRandom();
+            },
+            runChildUpdate: true
         });
-*/
-/*        this.time.addEvent({
-            delay: 100,
+
+        this.time.addEvent({
+            delay: 5000,
+
             loop: true,
-            callback: this.addAlien
-        });*/
+            callback: () => this.addCloud()
+        });
+
+    }
+
+    update(t: number, dt: number) {
+        
     }
 
 
-    private addAlien() {
-        // Random position above screen
-        const x = Phaser.Math.Between(250, 800);
-        const y = Phaser.Math.Between(-64, 0);
-
-        // Find first inactive sprite in group or add new sprite, and set position
-        const alien = this.group.get(x, y);
+    private addCloud(): void {
+        // Find first inactive sprite in group or add new sprite
+        const cloud = this.group.get();
 
         // None free or already at maximum amount of sprites in group
-        if (!alien) return;
-
+        if (!cloud) return;
     }
 }
--- src/sprites/Cloud.ts
+++ src/sprites/Cloud.ts
@@ -1,29 +1,31 @@
 import Phaser from 'phaser'
 
-declare global {
-    namespace Phaser.GameObjects {
-        interface GameObjectFactory {
-            cloud(): Cloud;
-        }
-    }
-}
-
+const KEY = 'clouds';
 export default class Cloud extends Phaser.Physics.Arcade.Sprite {
 
 
-
     private speed: number = 50;
-    // constructor(scene: Phaser.Scene, x: number, y: number, key: string, frame: string | number) {
 
-    //     super(scene, x, y, key, frame);
-    // }
-    constructor(scene: Phaser.Scene, x: number, y: number, key: string, frame: string | number) {
-
-        super(scene, x, y, key, frame);
+    constructor(scene: Phaser.Scene, x: number, y: number, key: string, frame?: string | number) {
+        super(scene, x, y, KEY, frame);
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
     }
 
     createRandom() {
-
+        // get metadata aboud the clouds from the clouds.json file
+        if (!this.scene) {
+            return
+        }
+        let cloudMetadata: Phaser.Textures.Texture = this.scene.textures.get(KEY);
+        // from the clouds.json there is only 1 texture element so the index should be 0
+        let frames: Phaser.Textures.Frame[] = cloudMetadata.getFramesFromTextureSource(0);
+        let frame: Phaser.Textures.Frame = frames[Phaser.Math.Between(0, frames.length - 1)];
+        let x = Number(this.scene.game.config.width) + frame.width;
+        let y = Phaser.Math.Between(0, Number(this.scene.game.config.height));
+        this.setX(x);
+        this.setY(y);
+        this.setFrame(frame.name);
     }
 
     preUpdate(t: number, dt: number) {
@@ -31,31 +33,11 @@ export default class Cloud extends Phaser.Physics.Arcade.Sprite {
         this.setVelocity(-this.speed, 0);
     }
 
-}
-
-const cloudTypes = ['cumulus-huge', 'cumulus-big1', 'cumulus-big2', 'cumulus-big3', 'cumulus-small1', 'cumulus-small2', 'cumulus-small3'];
-const KEY = 'clouds';
-
-Phaser.GameObjects.GameObjectFactory.register('cloud', function (this: Phaser.GameObjects.GameObjectFactory) {
-    // let randCoeff1 = Math.random() * 2;
-    // let randCoeff2 = Math.random() * 0.3; // separate random in order to limit huge clouds
-    // let randomType = Math.floor(randCoeff1 + randCoeff2);
-
-    // get metadata aboud the clouds from the clouds.json file
-    if (!this.scene) {
-        return 
+    update(t: number, dt: number) {
+        super.update(t, dt);
+        if (this.x < -(this.width/2)) {
+            this.destroy();
+        }
     }
-    let cloudMetadata: Phaser.Textures.Texture = this.scene.textures.get(KEY);
-    // from the clouds.json there is only 1 texture element so the index should be 0
-    let frames: Phaser.Textures.Frame[] = cloudMetadata.getFramesFromTextureSource(0);
-    let frame: Phaser.Textures.Frame = frames[Phaser.Math.Between(0, frames.length)];
-    let x = Number(this.scene.game.config.width) + frame.width;
-    let y = Phaser.Math.Between(0, Number(this.scene.game.config.height));
-    let cloud = new Cloud(this.scene, x, y, KEY, frame.name);
 
-    this.displayList.add(cloud);
-    this.updateList.add(cloud);
-    this.scene.physics.world.enableBody(cloud, Phaser.Physics.Arcade.DYNAMIC_BODY);
-
-    return cloud;
-})
+}
