6d64ed8f2830ed2b78c9b573c1460bf3a5ca3680
Kostas Paganelis <paganelis@gmail.com>
2021-06-19 01:38:33 +0300

    Refactored cloud creation

--- src/scenes/CloudScene.ts
+++ src/scenes/CloudScene.ts
@@ -1,8 +1,10 @@
 import Phaser from 'phaser'
 import Cloud from '../sprites/Cloud'
 
+const KEY = 'clouds';
+
 export default class CloudScene extends Phaser.Scene {
-    private group!: Phaser.Physics.Arcade.Group;
+    private cloudGroup!: Phaser.Physics.Arcade.Group;
 
     constructor() {
         super('cloud-scene')
@@ -12,19 +14,15 @@ export default class CloudScene extends Phaser.Scene {
     }
 
     create() {
-        this.group = this.physics.add.group({
+        this.cloudGroup = this.physics.add.group({
             classType: Cloud,
             maxSize: 5,
-            createCallback: (item: Phaser.GameObjects.GameObject) => {
-                let cloud = item as Cloud;
-                cloud.createRandom();
-            },
             runChildUpdate: true
         });
 
         this.time.addEvent({
+            startAt:0,
             delay: 5000,
-
             loop: true,
             callback: () => this.addCloud()
         });
@@ -37,10 +35,15 @@ export default class CloudScene extends Phaser.Scene {
 
 
     private addCloud(): void {
+         // get metadata aboud the clouds from the clouds.json file
+         let cloudMetadata: Phaser.Textures.Texture = this.textures.get(KEY);
+         // from the clouds.json there is only 1 texture element so the index should be 0
+         let frames: Phaser.Textures.Frame[] = cloudMetadata.getFramesFromTextureSource(0);
+         let frame: Phaser.Textures.Frame = frames[Phaser.Math.Between(0, frames.length - 1)];
+         let x = Number(this.game.config.width) + frame.width;
+         let y = Phaser.Math.Between(0, Number(this.game.config.height));
         // Find first inactive sprite in group or add new sprite
-        const cloud = this.group.get();
-
-        // None free or already at maximum amount of sprites in group
-        if (!cloud) return;
+        this.cloudGroup.create(x, y, KEY, frame.name);
     }
+
 }
--- src/sprites/Cloud.ts
+++ src/sprites/Cloud.ts
@@ -1,31 +1,32 @@
 import Phaser from 'phaser'
 
-const KEY = 'clouds';
+interface CloudConfig {
+    velocity: number;
+}
+
+const CLOUD_CONFIG: { [key: string]: CloudConfig } = {
+    'small': {
+        velocity: 300
+    },
+    'big': {
+        velocity: 100
+    },
+    'huge': { 
+        velocity: 50 
+    }
+};
 export default class Cloud extends Phaser.Physics.Arcade.Sprite {
 
 
-    private speed: number = 50;
+    private speed: number;
 
-    constructor(scene: Phaser.Scene, x: number, y: number, key: string, frame?: string | number) {
-        super(scene, x, y, KEY, frame);
-        scene.add.existing(this);
-        scene.physics.add.existing(this);
-    }
-
-    createRandom() {
-        // get metadata aboud the clouds from the clouds.json file
-        if (!this.scene) {
-            return
+    constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame: string) {
+        super(scene, x, y, texture, frame);
+        let type = frame.split('-')[1];
+        if (type !== 'huge') {
+            type = type.substring(0, type.length-1); // cut the last digit from the name
         }
-        let cloudMetadata: Phaser.Textures.Texture = this.scene.textures.get(KEY);
-        // from the clouds.json there is only 1 texture element so the index should be 0
-        let frames: Phaser.Textures.Frame[] = cloudMetadata.getFramesFromTextureSource(0);
-        let frame: Phaser.Textures.Frame = frames[Phaser.Math.Between(0, frames.length - 1)];
-        let x = Number(this.scene.game.config.width) + frame.width;
-        let y = Phaser.Math.Between(0, Number(this.scene.game.config.height));
-        this.setX(x);
-        this.setY(y);
-        this.setFrame(frame.name);
+        this.speed = CLOUD_CONFIG[type].velocity + 100 * Math.random();
     }
 
     preUpdate(t: number, dt: number) {
@@ -35,7 +36,7 @@ export default class Cloud extends Phaser.Physics.Arcade.Sprite {
 
     update(t: number, dt: number) {
         super.update(t, dt);
-        if (this.x < -(this.width/2)) {
+        if (this.x < -(this.width / 2)) {
             this.destroy();
         }
     }
